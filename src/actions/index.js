db2e97c21faf691e2a3808305b5188db5fe603cf
Nalaka Vithanage <N.Vithanage@shsc.nhs.uk>
2020-04-26 10:04:09 +0100

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import jsonPlaceholder from'../apis/jsonPlaceholder';
+export const fetchPosts = async () =>  async dispatch => {
+   const response = await jsonPlaceholder.get('/posts');
+    
+   dispatch({ type: 'FETCH_POSTS' , payload: response})
+   // return{
+  //    type: 'FETCH_POSTS',
+ //     payload: response
+};
