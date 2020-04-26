9a978ca9d1d17ac969ee451b98fceba566a0bdf6
Nalaka Vithanage <N.Vithanage@shsc.nhs.uk>
2020-04-26 13:48:54 +0100

    Error fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import jsonPlaceholder from'../apis/jsonPlaceholder';
-export const fetchPosts = async () =>  async dispatch => {
+export const fetchPosts =  () =>  async dispatch => {
    const response = await jsonPlaceholder.get('/posts');
     
    dispatch({ type: 'FETCH_POSTS' , payload: response})
